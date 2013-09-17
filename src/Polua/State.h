f1b54923659fa08d1385e7d209506aa0470489c6
lhamot <loic.hamot@yahoo.fr>
2013-09-17 19:51:24 +0000

    Si un lue_error est lancé hors d'un lua_call, cela lance une exception (au lieu de cracher...)

--- src/Polua/State.h
+++ src/Polua/State.h
@@ -22,11 +22,22 @@ class State
 
 	lua_State* const state_;
 
+	//! Appelée par lua pour gerer une erreur quand on est PAS dans un call
+	static int at_panic(lua_State* L)
+	{
+		char const* mess = lua_tostring(L, -1);
+		std::string message = mess ? mess : "lua error without message";
+		lua_pop(L, 1);
+		throw Polua::Exception(0, message);
+	}
+
 public:
 	State(): state_(luaL_newstate())
 	{
 		if(state_ == NULL)
 			throw std::bad_alloc();
+
+		lua_atpanic(state_, &State::at_panic);
 	}
 
 	~State()
